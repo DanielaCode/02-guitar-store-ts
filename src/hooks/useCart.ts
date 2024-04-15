58015b1ad71cc354de2a9df3245e3857895d6c6a
Daniela <danielagp96@gmail.com>
2024-04-15 17:58:44 -0600

    Creating a type with a lock up from guitar id and correcting the type of storagecart, customhook type done

--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -8,7 +8,7 @@ import type { Guitar,CartItem } from "../types";
 //allows to do testing easier
 function useCart() {
 
-    const storageInitialCart = () : Guitar[]=> {
+    const storageInitialCart = () : CartItem[]=> {
         const localStorageCart = localStorage.getItem("cart");
         return localStorageCart ? JSON.parse(localStorageCart) : [];
     }
@@ -52,13 +52,13 @@ function useCart() {
     }
 
 
-    function removeItem(id) {
+    function removeItem(id:Guitar["id"]) {
         setCart(prevCart => prevCart.filter((e) => e.id !== id));
         //the set function already knows what is in the state
         //and you can used it via callback as it is avoid and the parameter is the previous cart
     }
 
-    function increaseQuantity(id) {
+    function increaseQuantity(id:Guitar["id"]) {
         const updatedCart = cart.map((e) => {
             if (e.id === id && e.quantity < 5) {
                 return {
@@ -75,7 +75,7 @@ function useCart() {
         setCart([]);
     }
 
-    function decreaseQuantity(id) {
+    function decreaseQuantity(id:Guitar["id"]) {
         const updatedCart = cart.map((e) => {
             if (e.id === id && e.quantity > 1) {
                 return {
